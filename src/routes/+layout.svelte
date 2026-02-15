888c396f8b9b48df44ac44e379900cdcdd4105f3
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2026-02-15 19:02:24 +0800

    what

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,64 @@
 <script lang="ts">
-	import './layout.css';
+  import "carbon-components-svelte/css/all.css";
 
-	const { children } = $props();
+  import {
+    Column,
+    Content,
+    Grid,
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    Row,
+    SideNav,
+    SideNavDivider,
+    SideNavItems,
+    SideNavLink,
+    SideNavMenu,
+    SideNavMenuItem,
+    SkipToContent,
+    Tabs,
+    Tab,
+    TabContent,
+  } from "carbon-components-svelte";
+  import Upload from "carbon-icons-svelte/lib/Upload.svelte";
+  import Graph from "carbon-icons-svelte/lib/Network_2.svelte";
+
+  import "./layout.css";
+
+  const { children } = $props();
+
+  let isSideNavOpen = $state(false);
 </script>
 
-{@render children()}
+<Header companyName="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+  <svelte:fragment slot="skipToContent">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem>
+      <p>foo</p>
+    </HeaderNavItem>
+  </HeaderNav>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen} rail>
+  <SideNavItems>
+    <SideNavLink icon={Upload} on:click={() => alert(1)}>Upload</SideNavLink>
+    <SideNavLink icon={Graph} on:click={() => alert(1)}>DFA</SideNavLink>
+    <SideNavLink icon={Graph} on:click={() => alert(1)}>PDA</SideNavLink>
+    <SideNavLink icon={Graph} on:click={() => alert(1)}>
+      Turing Machine
+    </SideNavLink>
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        {@render children()}
+      </Column>
+    </Row>
+  </Grid>
+</Content>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,123 @@
+import autoImport from "unplugin-auto-import";
+import { purgePolyfills } from "unplugin-purge-polyfills";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    autoImport.vite({
+      imports: [
+        {
+          from: "ts-pattern",
+          imports: ["match", "P"],
+        },
+        {
+          from: "es-toolkit/array",
+          imports: [
+            "at",
+            "chunk",
+            "compact",
+            "countBy",
+            "difference",
+            "differenceBy",
+            "differenceWith",
+            "drop",
+            "dropRight",
+            "dropRightWhile",
+            "dropWhile",
+            "fill",
+            "flatMap",
+            "flatMapDeep",
+            "flatten",
+            "flattenDeep",
+            "groupBy",
+            "head",
+            "initial",
+            "intersection",
+            "intersectionBy",
+            "intersectionWith",
+            "isSubset",
+            "isSubsetWith",
+            "keyBy",
+            "last",
+            "maxBy",
+            "minBy",
+            "orderBy",
+            "partition",
+            "pull",
+            "pullAt",
+            "remove",
+            "sample",
+            "sampleSize",
+            "shuffle",
+            "sortBy",
+            "tail",
+            "take",
+            "takeRight",
+            "takeRightWhile",
+            "takeWhile",
+            "toFilled",
+            "union",
+            "unionBy",
+            "unionWith",
+            "uniq",
+            "uniqBy",
+            "uniqWith",
+            "unzip",
+            "unzipWith",
+            "windowed",
+            "without",
+            "xor",
+            "xorBy",
+            "xorWith",
+            "zip",
+            "zipObject",
+            "zipWith",
+          ],
+        },
+        {
+          from: "es-toolkit/function",
+          imports: [
+            "flow",
+            "flowRight",
+            "partial",
+            "partialRight",
+            "rest",
+            "spread",
+          ],
+        },
+        {
+          from: "es-toolkit/object",
+          imports: [
+            "clone",
+            "cloneDeep",
+            "cloneDeepWith",
+            "findKey",
+            "flattenObject",
+            "invert",
+            "mapKeys",
+            "mapValues",
+            "merge",
+            "mergeWith",
+            "omit",
+            "omitBy",
+            "pick",
+            "pickBy",
+            "toCamelCaseKeys",
+            "toMerged",
+            "toSnakeCaseKeys",
+          ],
+        },
+      ],
+    }),
+    tailwindcss(),
+    sveltekit(),
+    purgePolyfills.vite({}),
+  ],
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
