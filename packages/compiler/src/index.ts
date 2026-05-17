ace1aefdd64afaa01ef24bc6fa5423781752fc49
gekkotadev <14091010+GekkotaDev@users.noreply.github.com>
2026-05-17 19:16:20 +0800

    https://youtu.be/2EfvHUs_EgY

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,6 +1,10 @@
 import { lexString } from "~/lexer/lexer";
 import { parse } from "~/ast/ast";
 
+export { lexString, lex } from "~/lexer/lexer";
+export { parse } from "~/ast/ast";
+export type { Node } from "~/ast/types";
+
 const string = "(aa+bb)(a+b)*";
 
 console.log(string);
